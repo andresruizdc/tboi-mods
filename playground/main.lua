a2c6c058bb31d4a982ea28c724ca1b5cd5a5346c
Andres Ruiz <andrsruiz@gmail.com>
2020-10-31 12:57:56 -0600

    Try to get room ID, not working

--- playground/main.lua
+++ playground/main.lua
@@ -1,8 +1,8 @@
 -- Register the mod in the API (dont change anything here, except the name)
 local mod = RegisterMod("Dark Matter tears", 1)
+player = Isaac.GetPlayer(0) -- get the player entity
 
 function mod:onTear(tear)
-	local player = Isaac.GetPlayer(0) --get the player entity
 	player:AddCoins(1) -- add a coin
 	player:AddBombs(1) -- add a bomb
 	player:AddKeys(1) -- add a key
@@ -14,12 +14,20 @@ end
 
 -- prints the player position.
 function mod:onPlayerMoved()
-	local player = Isaac.GetPlayer(0)
 	local pos = player.Position
 	Isaac.RenderText("X: "..pos.X.." Y: "..pos.Y, 50, 50, 1, 1, 1, 1)
+end
+
+-- Room
+function mod:onPlayerEnterRoom()
+	Game():GetRoom():SetWallColor(Color(1,1,1,1,255,0,0))
+	local roomId = Game():GetRoom().GetType()
+	Isaac.RenderText("Game"..roomId, 50, 75, 1, 1, 1, 1)
 end
 
 -- Trigger the function "onTear()", when the "POST_FIRE_TEAR" callback is triggered.
 mod:AddCallback(ModCallbacks.MC_POST_FIRE_TEAR, mod.onTear)
 -- Trigger the function "onPlayerMoved()" whtn the "POST_PLAYER_RENDER" callback is triggered.
 mod:AddCallback(ModCallbacks.MC_POST_PLAYER_RENDER, mod.onPlayerMoved)
+-- Trigger the function "onPlayerEnterRoom()" with the "POST_NEW_ROOM" callback is triggered.
+mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, mod.onPlayerEnterRoom)
