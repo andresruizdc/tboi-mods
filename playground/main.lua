f4104901e1f20fd1e58ed8b2a662c6d8ebfa8a9e
Andres Ruiz <andrsruiz@gmail.com>
2020-11-13 19:54:02 -0600

    Spawn enemy on active item used

--- playground/main.lua
+++ playground/main.lua
@@ -35,11 +35,20 @@ end
 -- Check room ID, if it's a Store it'll spawn an item.
 -- Also it changes the wall and floor color.
 function mod:onPlayerEnterRoom()
+	local player = Isaac.GetPlayer(0)
 	Game():GetRoom():SetWallColor(Color(1,1,1,1,0,255,0))
 	Game():GetRoom():SetFloorColor(Color(1,1,1,1,0,0,255))
 	if Game():GetRoom():GetType() == RoomType.ROOM_SHOP then
 		Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, 0, Vector(320, 280), Vector(0, 0), nil)
+	else
+		Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_LIL_BATTERY, 0, player.Position, Vector(0, 0), nil)
 	end
+end
+
+-- Spawn a Monstro when active item is used
+function mod:onItemUsed()
+	local player = Isaac.GetPlayer(0)
+	Isaac.Spawn(EntityType.ENTITY_MONSTRO, 1, 0, Vector(320, 280), Vector(0, 0), player)
 end
 
 -- Trigger the function "onTear()", when the "POST_FIRE_TEAR" callback is triggered.
@@ -52,3 +61,5 @@ mod:AddCallback(ModCallbacks.MC_POST_TEAR_RENDER, mod.renderTearPosition)
 mod:AddCallback(ModCallbacks.MC_POST_TEAR_UPDATE, mod.makeExplosiveTrajectory)
 -- Trigger the function "onPlayerEnterRoom()", when the "POST_NEW_ROOM" callback is triggered.
 mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, mod.onPlayerEnterRoom)
+-- Trigger the function "onItemUsed()", when the "USE_ITEM" callback is triggered.
+mod:AddCallback(ModCallbacks.MC_USE_ITEM, mod.onItemUsed)
