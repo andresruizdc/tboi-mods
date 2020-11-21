d203162f7ae619e11b1e4e62aa599fef68615a0c
Andres Ruiz <andrsruiz@gmail.com>
2020-11-20 20:07:24 -0600

    Set Cache Callback for picking up items

--- drips/main.lua
+++ drips/main.lua
@@ -68,7 +68,45 @@ function mod:onUpdate(player)
 	UpdateDrips(player)
 end
 
+-- Update the Cache
+function mod:onCache(player, cacheFlag)
+	if cacheFlag == CacheFlag.CACHE_DAMAGE then
+		if player:HasCollectible(DripId.SALINE) and not HasDrip.saline then
+			if player.MaxFireDelay >= MIN_TEAR_DELAY + DripBonus.SALINE then
+				player.MaxFireDelay = player.MaxFireDelay - DripBonus.SALINE
+			elseif player.MaxFireDelay >= MIN_TEAR_DELAY then
+				player.MaxFireDelay = MIN_TEAR_DELAY
+			end
+		end
+		if player:HasCollectible(DripId.SLUDGE) then
+			player.Damage = player.Damage + DripBonus.SLUDGE
+		end
+	elseif cacheFlag == CacheFlag.CACHE_SHOTSPEED then
+		if player:HasCollectible(DripId.POTASSIUM) then
+			player.ShotSpeed = player.ShotSpeed + DripBonus.POTASSIUM
+		end
+	elseif cacheFlag == CacheFlag.CACHE_RANGE then
+		if player:HasCollectible(DripId.GLUCOSE) then
+			player.TearHeight = player.TearHeight + DripBonus.GLUCOSE_TEAR_HEIGHT
+			player.TearFallingSpeed = player.TearFallingSpeed + DripBonus.GLUCOSE_FALL_SPEED
+		end
+	elseif cacheFlag == CacheFlag.CACHE_SPEED then
+		if player:HasCollectible(DripId.GLUCOSE) then
+			player.MoveSpeed = player.MoveSpeed + DripBonus.GLUCOSE_SPEED
+		end
+	elseif cacheFlag == CacheFlag.CACHE_LUCK then
+		if player:HasCollectible(DripId.DEW) then
+			player.Luck = player.Luck + DripBonus.DEW
+		end
+	elseif cacheFlag == CacheFlag.CACHE_FLYING then
+		if player:HasCollectible(DripId.HELIUM) then
+			player.CanFly = true
+		end
+	end
+end
+
 -- Callbacks
 -- TODO Check how to use Item Pools and add the item there.
 mod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, mod.onPlayerInit)
 mod:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, mod.onUpdate)
+mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, mod.onCache)
